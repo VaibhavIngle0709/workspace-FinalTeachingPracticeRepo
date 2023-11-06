0cb32c745d90a4471ba8b70a6d0a0e2c516419e2
Iamneo <techadmin@iamneo.ai>
2023-11-06 09:23:39 +0000

    Backup Commit

---
+++ EMSApi/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EMSApi.Models;
+
+public partial class Employee
+{
+    public int EmpId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Salary { get; set; }
+
+    public int Id { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public DateTime Dob { get; set; }
+
+    public virtual Department IdNavigation { get; set; } = null!;
+}
