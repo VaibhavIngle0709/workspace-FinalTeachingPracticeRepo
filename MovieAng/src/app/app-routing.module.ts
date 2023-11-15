7ccac489cd09422f47004bff7523270408e642fb
Iamneo <techadmin@iamneo.ai>
2023-11-15 11:32:15 +0000

    Backup Commit

--- MovieAng/src/app/app-routing.module.ts
+++ MovieAng/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListMovieComponent } from './list-movie/list-movie.component';
+import { FindmovieComponent } from './findmovie/findmovie.component';
 
 
 const routes: Routes = [
-  
+  {path:'listmovies',component:ListMovieComponent},
+  {path:'find/:id',component:FindmovieComponent}
 ];
 
 @NgModule({
