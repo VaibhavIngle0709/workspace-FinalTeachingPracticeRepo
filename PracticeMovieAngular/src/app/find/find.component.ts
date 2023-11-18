8f0fb29f63624e102e1291b20d5de479eae74f55
Iamneo <techadmin@iamneo.ai>
2023-11-18 06:38:17 +0000

    Backup Commit

--- PracticeMovieAngular/src/app/find/find.component.ts
+++ PracticeMovieAngular/src/app/find/find.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieserviceService } from '../services/movieservice.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IMovie } from '../model/imovie';
 import { IDetail } from '../model/idetail';
 
@@ -11,17 +11,21 @@ import { IDetail } from '../model/idetail';
 })
 export class FindComponent implements OnInit {
 
-  constructor(private ms:MovieserviceService,private ar:ActivatedRoute) { }
+  constructor(private ms:MovieserviceService,private ar:ActivatedRoute,private route:Router) { }
   moviedata:IMovie
-  moviedetail:IDetail
+  //moviedetail:IDetail
   id:number
 
-
+  Save()
+  {
+    this.route.navigate(['\listmovies'])
+  }
   ngOnInit() {
     const tid=this.ar.snapshot.paramMap.get('id')
     this.id=Number(tid)
-    this.ms.getMovie(this.id).subscribe((data:IMovie)=>{this.moviedata=data})
-    this.ms.getDetail(this.id).subscribe((data:IDetail)=>{this.moviedetail=data})
+    this.ms.getMovie(this.id).subscribe((data:IMovie)=>{this.moviedata=data;console.log(this.moviedata)})
+
+   // this.ms.getDetail(this.id).subscribe((data2:IDetail)=>{this.moviedetail=data2;console.log(this.moviedetail)})
   }
 
 }
